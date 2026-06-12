153835c36a1fcab540f17bacf24c28f8d62a4c81
boat1994 <36528988+boat1994@users.noreply.github.com>
2026-06-12 13:42:45 +0700

    Add project admin view button stub

---
+++ src/components/ViewProjectButton/index.tsx
@@ -0,0 +1,9 @@
+'use client'
+
+export function ViewProjectButton() {
+  return (
+    <div style={{ fontSize: 12, opacity: 0.8 }}>
+      View project link is available on the public project page after save.
+    </div>
+  )
+}
